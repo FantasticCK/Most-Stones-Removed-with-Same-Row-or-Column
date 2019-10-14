e32564788f3cb2cc7f211a7f592fa70a97edbf98
CK <kainingc@usc.edu>
2019-10-14 11:18:16 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,77 @@
+package com.CK;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] stones = {{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}};
+        new Solution().removeStones(stones);
+    }
+}
+
+class Solution1 {
+    Map<Integer, Integer> f = new HashMap<>();
+    int islands = 0;
+
+    public int removeStones(int[][] stones) {
+        for (int i = 0; i < stones.length; ++i)
+            union(stones[i][0], ~stones[i][1]);
+        return stones.length - islands;
+    }
+
+    public int find(int x) {
+        if (f.putIfAbsent(x, x) == null)
+            islands++;
+        if (x != f.get(x))
+            f.put(x, find(f.get(x)));
+        return f.get(x);
+    }
+
+    public void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            f.put(x, y);
+            islands--;
+        }
+    }
+}
+
+class Solution {
+    public int removeStones(int[][] stones) {
+        int N = stones.length;
+        DSU dsu = new DSU(20000);
+
+        for (int[] stone : stones)
+            dsu.union(stone[0], stone[1] + 10000);
+
+        Set<Integer> seen = new HashSet();
+        for (int[] stone : stones)
+            seen.add(dsu.find(stone[0]));
+
+        return N - seen.size();
+    }
+}
+
+class DSU {
+    int[] parent;
+
+    public DSU(int N) {
+        parent = new int[N];
+        for (int i = 0; i < N; ++i)
+            parent[i] = i;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        parent[find(x)] = find(y);
+    }
+}
